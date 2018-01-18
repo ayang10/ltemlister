0826355c6811eb516371d8cbfac694b8b286ac4b
ayang10 <ayang014@gmail.com>
2018-01-18 16:49:43 -0500

    finish first itemlister react native app

--- app/components/ToolBar/ToolBar.js
+++ app/components/ToolBar/ToolBar.js
@@ -9,12 +9,6 @@ import {
 } from 'react-native';
 const styles = require('../../style');
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class Toolbar extends Component {
   render() {
